48aa6b13357632b3558313bfaf19ce0738a6ffe2
joege <joe.ge@alliedvision.com>
2021-09-27 16:40:27 +0800

    refine delete-old-bmp.sh

--- SVCamMiniSampleGui64-SimpleSWTrigger/MiniSampleGui/Image/delete-old-bmp.sh
+++ SVCamMiniSampleGui64-SimpleSWTrigger/MiniSampleGui/Image/delete-old-bmp.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 while true
 do
+	# List bmp files to be deleted
 	find . -mmin 1 -name "*.bmp" 
+	# Remove bmp files old than 1 minute
 	find . -mmin 1 -name "*.bmp" -delete
-	sleep 10s
+
+	sleep 10s 
 done
